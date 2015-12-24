783aaa842bbd3466d05c674bf3771d06476ca2bc
木白 <624104747@qq.com>
2015-12-24 16:44:31 +0800

    恢复热词接口

--- src/main/java/sjes/elasticsearch/controller/SearchLogController.java
+++ src/main/java/sjes/elasticsearch/controller/SearchLogController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import sjes.elasticsearch.common.ServiceException;
+import sjes.elasticsearch.domain.HotWordModel;
 import sjes.elasticsearch.service.SearchLogService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("logs")
@@ -30,5 +33,16 @@ public class SearchLogController {
     @RequestMapping(method = RequestMethod.PUT)
     public void index(String keyword, Long categoryId, String shopId, String sortType, Double startPrice, Double endPrice, String userAgent, String ip) throws ServiceException {
         searchLogService.index(keyword, categoryId, shopId, sortType, startPrice, endPrice, userAgent, ip);
+    }
+
+    /**
+     * 获取热门搜索词
+     *
+     * @param maxCount 热门搜索词的最大数量，默认为5
+     * @return 热门搜索词
+     */
+    @RequestMapping(value = "hotwords", method = RequestMethod.GET)
+    public List<HotWordModel> getHotWords(Integer maxCount) throws ServiceException {
+        return searchLogService.getHotWords(maxCount);
     }
 }
