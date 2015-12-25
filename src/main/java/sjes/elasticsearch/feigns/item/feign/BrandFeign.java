c62a122923c3cd43e73d8d4ed4fce9076e9840d8
qinhailong <qinhailong@sanjiang365.com>
2015-12-25 13:10:26 +0800

    添加查询品牌列表

---
+++ src/main/java/sjes/elasticsearch/feigns/item/feign/BrandFeign.java
@@ -0,0 +1,25 @@
+package sjes.elasticsearch.feigns.item.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import sjes.elasticsearch.constants.Constants;
+import sjes.elasticsearch.feigns.item.model.Brand;
+
+import java.util.List;
+
+/**
+ * Created by qinhailong on 15-12-25.
+ */
+@FeignClient(Constants.SJES_API_ITEM)
+@RequestMapping(value = "brands")
+public interface BrandFeign {
+
+    /**
+     * 查询所有品牌信息
+     * @return 品牌列表
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    List<Brand> listAll();
+
+}
---
+++ src/main/java/sjes/elasticsearch/service/BrandService.java
@@ -0,0 +1,29 @@
+package sjes.elasticsearch.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import sjes.elasticsearch.feigns.item.feign.BrandFeign;
+import sjes.elasticsearch.feigns.item.model.Brand;
+
+import java.util.List;
+
+/**
+ * Created by qinhailong on 15-12-25.
+ */
+@Service("brandService")
+public class BrandService {
+
+    @Autowired
+    private BrandFeign brandFeign;
+
+    /**
+     * 查询所有品牌信息
+     * @return 品牌列表
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Brand> listAll() {
+        return brandFeign.listAll();
+    }
+}
