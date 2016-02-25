1abaa9070d345ed95902592192908d766afd3bac
木白 <624104747@qq.com>
2016-02-25 14:55:30 +0800

    增加定时更新索引功能

--- src/main/java/sjes/elasticsearch/ElasticSearchApplication.java
+++ src/main/java/sjes/elasticsearch/ElasticSearchApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author qinhailong
  */
 @SpringBootApplication
+@EnableScheduling
 @EnableDiscoveryClient
 @EnableFeignClients("sjes.elasticsearch.feigns")
 public class ElasticSearchApplication {
