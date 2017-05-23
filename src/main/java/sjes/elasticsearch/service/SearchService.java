36e7e830b569733cde46a2e618647ae35a8eee85
fudongwei <fudongwei@sanjiang365.com>
2017-05-23 10:48:23 +0800

    [ADD]: 增加根据管理编码分页查询商品信息接口

--- src/main/java/sjes/elasticsearch/service/SearchService.java
+++ src/main/java/sjes/elasticsearch/service/SearchService.java
@@ -531,9 +531,9 @@ public class SearchService {
         org.springframework.data.domain.Pageable pageable = new PageRequest(page - 1, size);
         if (CollectionUtils.isNotEmpty(erpGoodsIds)) {
             Page<ProductIndex> productIndexPage = productIndexRepository.findByErpGoodsIdIn(erpGoodsIds, pageable);
-            return new PageModel<>(productIndexPage.getContent(), productIndexPage.getTotalElements(), new Pageable(page - 1, size));
+            return new PageModel<>(productIndexPage.getContent(), productIndexPage.getTotalElements(), new Pageable(page, size));
         }
-        return new PageModel<>(null, 0, new Pageable(page - 1, size));
+        return new PageModel<>(null, 0, new Pageable(page, size));
     }
 
     /**
